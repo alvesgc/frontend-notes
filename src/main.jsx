c7a5f39e0b56b9bb02b649f48c8c8eaca8482baf
Alisson <alissonalves64@hotmail.com>
2023-11-23 20:45:06 -0300

    organizando styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Details } from './pages/Details'
 
+import theme from './styles/theme'
+
+import { ThemeProvider } from 'styled-components';
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <React.StrictMode theme={theme}>
+    <ThemeProvider>
     <Details />
+    </ThemeProvider>
   </React.StrictMode>,
 )
