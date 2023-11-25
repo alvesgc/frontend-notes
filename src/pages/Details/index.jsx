ce368469efe42ec36b51ca95513b823f23912f57
Alisson <alissonalves64@hotmail.com>
2023-11-25 19:33:02 -0300

    :lipstick: style: Children element and links

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,23 @@
-import { Container } from "./styles";
+import { Container, Links } from "./styles";
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button"; 
+import { Section } from "../../components/Section"; 
 
 export function Details() {
   return (
     <Container>
       <Header />
+      <Section title="links-uteis">
+        <Links>
+          <li>
+            <a href="#">https://instagram.com/al___vs</a>
+          </li>
+          
+          <li>
+          <a href="#">https://instagram.com/al___vs</a>
+          </li>
+        </Links>
+      </Section>
       <Button title="Voltar" loading />
     </Container>
   )
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -8,5 +8,17 @@ display: grid;
 grid-template-rows: 105px auto;
 grid-template-areas: 
 "header"
-"content"
+"content";
 `
+
+export const Links = styled.ul`
+list-style: none;
+
+> li {
+  margin-top: 12px;
+  
+  a {
+    color: ${({ theme }) => theme.COLORS.WHITE};
+  }
+}
+` 
