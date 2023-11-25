8abf2834e97aaef2ba455914aa390ccd45df4686
Alisson <alissonalves64@hotmail.com>
2023-11-25 18:19:46 -0300

    :lipstick: feat: Iniciando componentização

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,9 +1,12 @@
 import { Container } from "./styles";
+import { Button } from "../../components/Button"; 
 
 export function Details() {
   return (
     <Container>
       <h1>Olá Mundo</h1>
+
+      <Button />
     </Container>
   )
 }
