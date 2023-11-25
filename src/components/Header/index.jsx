0e446043abb78471d3b08861afbf747c89c3db98
Alisson <alissonalves64@hotmail.com>
2023-11-25 19:03:08 -0300

    :lipstick: style: Header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,16 @@
-import { Container } from "./styles";
+import { Container, Profile } from "./styles";
 
 export function Header() {
   return (
     <Container>
+      <Profile>
+        <img src="https://github.com/alvesgc.png"  alt="Foto do usuario"/>
+        
+        <div>
+          <span>Bem-vindo</span>
+          <strong>Alisson alves</strong>
+        </div>
+      </Profile>
     </Container>
   )
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,8 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.header `
   grid-area: header;
-  background: red;
-  
+
   height: 105px;
   width: 100%;
   
@@ -16,3 +15,29 @@ export const Container = styled.header `
 
   padding: 0 80px;
 `;
+
+export const Profile = styled.div`
+ display: flex;
+ align-items: center;
+ > img {
+  width: 56px;
+  height: 56px;
+  border-radius: 50%;
+ }
+ >div {
+  display: flex;
+  flex-direction: column;
+  margin-left: 16px;
+  line-height: 24px;
+
+  span {
+    font-size: 14px;
+    color: ${({ theme }) => theme.COLORS.GRAY_100};
+  }
+
+  strong {
+    font-size: 18px;
+    color: ${({ theme }) => theme.COLORS.WHITE};
+  }
+ }
+`
