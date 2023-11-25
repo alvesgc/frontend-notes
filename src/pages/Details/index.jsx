4a7bc33a68a12900f3e9167c6c294ebb159ea9c2
Alisson <alissonalves64@hotmail.com>
2023-11-25 19:44:20 -0300

    feat: tag section

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,23 +1,30 @@
 import { Container, Links } from "./styles";
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button"; 
-import { Section } from "../../components/Section"; 
+import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
 
 export function Details() {
   return (
     <Container>
       <Header />
-      <Section title="links-uteis">
+      <Section title="Links úteis">
         <Links>
           <li>
             <a href="#">https://instagram.com/al___vs</a>
           </li>
-          
+
           <li>
           <a href="#">https://instagram.com/al___vs</a>
           </li>
         </Links>
       </Section>
+
+      <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="nodejs" />
+      </Section>
+
       <Button title="Voltar" loading />
     </Container>
   )
