028bc00371e70c6aeccb1aea19b6135a3ddb700e
Alisson <alissonalves64@hotmail.com>
2023-11-27 21:16:02 -0300

    :lipstick: style

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,34 +1,45 @@
-import { Container, Links } from "./styles";
+import { Container, Links, Content } from "./styles";
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button"; 
 import { Section } from "../../components/Section";
 import { ButtonText } from "../../components/ButtonText"; 
 import { Tag } from "../../components/Tag";
+
 export function Details() {
   return (
     <Container>
       <Header />
 
-      <ButtonText title="Excluir nota"/>
-
-      <Section title="Links úteis">
-        <Links>
-          <li>
+      <main>
+      <Content>
+        <ButtonText title="Excluir nota"/>
+        <h1>Titulo da nota</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Rerum vero minima praesentium ipsum quod esse neque suscipit, 
+          fuga unde culpa voluptatem vel repudiandae, provident nulla ipsa mollitia, 
+          ab labore! Nostrum?
+        </p>
+        <Section title="Links úteis">
+         <Links>
+            <li>
             <a href="#">https://instagram.com/al___vs</a>
-          </li>
+            </li>
 
-          <li>
-          <a href="#">https://instagram.com/al___vs</a>
-          </li>
-        </Links>
-      </Section>
+            <li>
+            <a href="#">https://instagram.com/al___vs</a>
+            </li>
+         </Links>
+        </Section>
 
-      <Section title="Marcadores">
-        <Tag title="express" />
-        <Tag title="nodejs" />
-      </Section>
+        <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="nodejs" />
+        </Section>
 
-      <Button title="Voltar" loading />
-    </Container>
+        <Button title="Voltar" loading />
+      </Content>
+    </main>
+      </Container>
   )
 }
