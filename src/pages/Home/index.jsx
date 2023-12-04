5099dbff5d9b278670ef1bddf28a86227ec296d1
Alisson <alissonalves64@hotmail.com>
2023-12-04 20:31:28 -0300

    feat: button for new notes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,7 +3,9 @@ import { FiPlus, FiSearch } from 'react-icons/fi';
 import {Container, Brand, Menu, Search, Content, NewNote} from './styles';
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
+import { Note } from '../../components/Note';
 import { ButtonText } from '../../components/ButtonText';
+import { Section } from '../../components/Section';
 
 export function Home() {
   return (
@@ -25,7 +27,17 @@ export function Home() {
       </Search>
       
       <Content>
-
+        <Section title="Minhas notas">
+        <Note data={{title: "React",
+      tags:[
+        {
+          id: 1, name: "react",
+        },
+        {
+          id: 2, name: "rocket"
+        }
+      ]}} />
+        </Section>
       </Content>
 
       <NewNote>
