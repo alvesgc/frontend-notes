be82044e24eeda82a8b16eb7bbfb98523f4b7309
Alisson <alissonalves64@hotmail.com>
2023-12-05 19:28:30 -0300

    style: Profile image input and label

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Profile />
+      < Profile />
     </ThemeProvider>
   </React.StrictMode>,
 )
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,7 +1,8 @@
-import { Container, Form } from './styles';
-import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
+import { Container, Form, Avatar } from './styles';
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi';
 import { Input }  from '../../components/Input';
 import { Button }  from '../../components/Button';
+
 export function Profile () {
   return (
     <Container>
@@ -12,6 +13,19 @@ export function Profile () {
       </header>
 
       <Form>
+        <Avatar>
+          <img 
+          src="https://github.com/alvesgc.png" 
+          alt="Foto do usuário" />
+          <label htmlFor="Avatar">
+            <FiCamera />
+
+            <input
+            id="Avatar"
+            type="file"
+            />
+          </label>
+        </Avatar>
         <Input 
           placeholder="Nome"
           type="text"
