a3a912bb18ebb1389f278810978db6bac38751f3
Alisson <alissonalves64@hotmail.com>
2023-12-07 23:47:28 -0300

    feat: create routes for pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/global'
 import theme from './styles/theme'
+import { Routes } from './routes'
 
-import { New } from './pages/New'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      < New />
+      < Routes />
     </ThemeProvider>
   </React.StrictMode>,
 )
